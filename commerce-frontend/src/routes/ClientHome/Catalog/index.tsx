78d106b9983c01cbc2584f21e4f40ddbb963661f
Matheus Marinho de Espíndula <1774@ucdb.br>
2025-12-07 09:52:19 -0400

    (feat): capturando erro de requisição

--- commerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ commerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -2,16 +2,28 @@ import "./styles.css";
 import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextpage from "../../../components/ButtonNextPage";
-import * as productService from "../../../services/product-service";
+import { useEffect, useState } from "react";
+import type { ProductDTO } from "../../../models/product";
+import * as productService from "../../../services/product-service" 
 
 export default function Catalog() {
+
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(()=>{
+    productService.findAll()
+      .then(response => {
+        setProducts(response.data.content)
+      })
+  },[])
+
   return (
     <>
       <main>
         <section id="catalog-section" className="dsc-container">
           <SearchBar />
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            {productService.findAll().map((product) => (
+            {products.map((product) => (
               <CatalogCard key={product.id} product={product} />
             ))}
           </div>
--- commerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ commerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,18 +1,33 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from "../../../services/product-service";
 import "./styles.css";
+import { useEffect, useState } from "react";
+import type { ProductDTO } from "../../../models/product";
+import * as productService from "../../../services/product-service"
 
 export default function ProductDetails() {
-  let params = useParams();
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>()
+  const navigate = useNavigate();
+  const param = useParams();
+    useEffect(()=>{
+    productService.findById(Number(param.productId))
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/")
+      });
+  }, [])
+
   return (
     <>
       <main>
         <section id="product-details-section" className="dsc-container">
-          {product && <ProductDetailsCard product={product} />}
+          {product
+          && <ProductDetailsCard product={product} />}
+          
           <div className="dsc-btn-page-container">
             <ButtonPrimary name="Comprar" />
             <Link to="/">
